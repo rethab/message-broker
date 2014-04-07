a15bd80efe47d8d0a850a665b9041f22bf86db09
Reto Hablützel <rethab@rethab.ch>
2014-04-07 21:07:17 +0200

    testdata, setup, more tests

--- test/stomp-test.c
+++ test/stomp-test.c
@@ -14,7 +14,7 @@ void test_parse_command_unknown() {
     struct stomp_command cmd;
 
     // unknown command
-    char str[] = "FOO\nlogin: client-1\n\n\0";
+    char str[] = "FOO\nlogin: client-1\n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_UNKNOWN_COMMAND,
         parse_command(str, &cmd));
 }
@@ -23,36 +23,44 @@ void test_parse_command_connect() {
     struct stomp_command cmd;
 
     // regular command
-    char str1[] = "CONNECT\nlogin: client-1\n\n\0";
-    CU_ASSERT_EQUAL_FATAL(0,
-        parse_command(str1, &cmd));
+    char str1[] = "CONNECT\nlogin: client-1\n\n";
+    int parsed = parse_command(str1, &cmd);
+    CU_ASSERT_EQUAL_FATAL(0, parsed);
     CU_ASSERT_STRING_EQUAL_FATAL("CONNECT", cmd.name);
-    printf("Header: %s\n", cmd.headers->key);
     CU_ASSERT_STRING_EQUAL_FATAL("login", cmd.headers->key);
-    CU_ASSERT_STRING_EQUAL_FATAL("client-0", cmd.headers->val);
+    CU_ASSERT_STRING_EQUAL_FATAL("client-1", cmd.headers->val);
+    CU_ASSERT_PTR_NULL_FATAL(cmd.content);
+    
+    // regular command with whitespaces in header
+    char str2[] = "CONNECT\n login :  client-1  \n\n";
+    parsed = parse_command(str2, &cmd);
+    CU_ASSERT_EQUAL_FATAL(0, parsed);
+    CU_ASSERT_STRING_EQUAL_FATAL("CONNECT", cmd.name);
+    CU_ASSERT_STRING_EQUAL_FATAL("login", cmd.headers->key);
+    CU_ASSERT_STRING_EQUAL_FATAL("client-1", cmd.headers->val);
     CU_ASSERT_PTR_NULL_FATAL(cmd.content);
 
     // missing login header
-    char str2[] = "CONNECT\n\n\0";
+    char str3[] = "CONNECT\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_MISSING_HEADER,
-        parse_command(str2, &cmd));
+        parse_command(str3, &cmd));
 
     // no val for login header
-    char str3[] = "CONNECT\nlogin: \0";
+    char str4[] = "CONNECT\nlogin: \n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_INVALID_HEADER,
-        parse_command(str3, &cmd));
+        parse_command(str4, &cmd));
  
     // command expects no body
-    char str4[] = "CONNECT\nlogin: client-1\n\nhello\n\0";
-    CU_ASSERT_EQUAL_FATAL(STOMP_UNEXPECTED_BODY,
-        parse_command(str4, &cmd));
+    char str5[] = "CONNECT\nlogin: client-1\n\nhello\n\n";
+    CU_ASSERT_EQUAL_FATAL(STOMP_UNEXPECTED_CONTENT,
+        parse_command(str5, &cmd));
 }
 
 void test_parse_command_send() {
     struct stomp_command cmd;
 
     // regular command
-    char str1[] = "SEND\ntopic: stocks\n\nnew price: 33.4\n\n\0";
+    char str1[] = "SEND\ntopic: stocks\n\nnew price: 33.4\n\n";
     CU_ASSERT_EQUAL_FATAL(0,
         parse_command(str1, &cmd));
     CU_ASSERT_STRING_EQUAL_FATAL("SEND", cmd.name);
@@ -61,7 +69,7 @@ void test_parse_command_send() {
     CU_ASSERT_STRING_EQUAL_FATAL("new price: 33.4", cmd.content);
  
     // multiline regular command
-    char str2[] = "SEND\ntopic: stocks\n\no p: 23.4\nn p: 33.4\n\n\0";
+    char str2[] = "SEND\ntopic: stocks\n\no p: 23.4\nn p: 33.4\n\n";
     CU_ASSERT_EQUAL_FATAL(0,
         parse_command(str2,
                       &cmd));
@@ -71,16 +79,16 @@ void test_parse_command_send() {
     CU_ASSERT_STRING_EQUAL_FATAL("o p: 23.4\nn p: 33.4", cmd.content);
 
     // missing topic header
-    char str3[] = "SEND\n\nnew price: 33.4\n\0";
+    char str3[] = "SEND\n\nnew price: 33.4\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_MISSING_HEADER,
         parse_command(str3, &cmd));
 
     // no value for topic header
-    char str4[] = "SEND\ntopic:\n\n new price: 33.4\n\n\0";
+    char str4[] = "SEND\ntopic:\n\n new price: 33.4\n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_INVALID_HEADER,
         parse_command(str4, &cmd));
     // empty value for topic header
-    char str5[] = "SEND\ntopic: \n\n new price: 33.4\n\n\0";
+    char str5[] = "SEND\ntopic: \n\n new price: 33.4\n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_INVALID_HEADER,
         parse_command(str5, &cmd));
 }
@@ -89,7 +97,7 @@ void test_parse_command_subscribe() {
     struct stomp_command cmd;
 
     // regular command
-    char str1[] = "SUBSCRIBE\ndestination: stocks\n\n\0";
+    char str1[] = "SUBSCRIBE\ndestination: stocks\n\n";
     CU_ASSERT_EQUAL_FATAL(0,
         parse_command(str1, &cmd));
     CU_ASSERT_STRING_EQUAL_FATAL("SUBSCRIBE", cmd.name);
@@ -98,22 +106,22 @@ void test_parse_command_subscribe() {
     CU_ASSERT_PTR_NULL_FATAL(cmd.content);
  
     // missing topic header
-    char str2[] = "SUBSCRIBE\n\n\0";
+    char str2[] = "SUBSCRIBE\n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_MISSING_HEADER,
         parse_command(str2, &cmd));
 
     // no value for topic header
-    char str3[] = "SUBSCRIBE\ndestination:\n\n\0";
+    char str3[] = "SUBSCRIBE\ndestination:\n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_INVALID_HEADER,
         parse_command(str3, &cmd));
     // empty value for topic header
-    char str4[] = "SUBSCRIBE\ndestination: \n\n\0";
+    char str4[] = "SUBSCRIBE\ndestination: \n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_INVALID_HEADER,
         parse_command(str4, &cmd));
  
     // command expects no body
-    char str5[] = "SUBSCRIBE\ndestination: stocks\n\nhello\n\n\0";
-    CU_ASSERT_EQUAL_FATAL(STOMP_UNEXPECTED_BODY,
+    char str5[] = "SUBSCRIBE\ndestination: stocks\n\nhello\n\n";
+    CU_ASSERT_EQUAL_FATAL(STOMP_UNEXPECTED_CONTENT,
         parse_command(str5, &cmd));
 }
 
@@ -125,12 +133,12 @@ void test_parse_command_disconnect() {
     CU_ASSERT_EQUAL_FATAL(0, parse_command(str1, &cmd));
 
     // invalid header (wants none)
-    char str2[] = "DISCONNECT\nfoo: bar\n\n\0";
+    char str2[] = "DISCONNECT\nfoo: bar\n\n";
     CU_ASSERT_EQUAL_FATAL(STOMP_INVALID_HEADER,
         parse_command(str2, &cmd));
 
-    char str3[] = "DISCONNECT\n\nbody\n\n\0";
-    CU_ASSERT_EQUAL_FATAL(STOMP_UNEXPECTED_BODY,
+    char str3[] = "DISCONNECT\n\nbody\n\n";
+    CU_ASSERT_EQUAL_FATAL(STOMP_UNEXPECTED_CONTENT,
         parse_command(str3, &cmd));
 }
 
@@ -161,7 +169,7 @@ void test_create_command_error() {
 
     char* str;
     CU_ASSERT_EQUAL_FATAL(0, create_command(cmd, str));
-    CU_ASSERT_STRING_EQUAL_FATAL("ERROR\nmesage:failed\n\n\0", str);
+    CU_ASSERT_STRING_EQUAL_FATAL("ERROR\nmesage:failed\n\n", str);
 }
 
 void test_create_command_message() {
@@ -174,7 +182,7 @@ void test_create_command_message() {
 
     char* str;
     CU_ASSERT_EQUAL_FATAL(0, create_command(cmd, str));
-    CU_ASSERT_STRING_EQUAL_FATAL("MESSAGE\n\nhello world\n\n\0", str);
+    CU_ASSERT_STRING_EQUAL_FATAL("MESSAGE\n\nhello world\n\n", str);
 
     // two lines
     cmd.name = "MESSAGE";
@@ -182,7 +190,7 @@ void test_create_command_message() {
     cmd.content = "hello\n world";
 
     CU_ASSERT_EQUAL_FATAL(0, create_command(cmd, str));
-    CU_ASSERT_STRING_EQUAL_FATAL("MESSAGE\n\nhello\n world\n\n\0", str);
+    CU_ASSERT_STRING_EQUAL_FATAL("MESSAGE\n\nhello\n world\n\n", str);
 }
 
 void test_create_command_receipt() {
@@ -194,7 +202,7 @@ void test_create_command_receipt() {
 
     char* str;
     CU_ASSERT_EQUAL_FATAL(0, create_command(cmd, str));
-    CU_ASSERT_STRING_EQUAL_FATAL("RECEIPT\n\n\0", str);
+    CU_ASSERT_STRING_EQUAL_FATAL("RECEIPT\n\n", str);
 }
 
 
