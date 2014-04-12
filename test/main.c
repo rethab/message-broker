ff2bd70f46edd5800bbe7e31efc39aa64d31b81a
Reto Hablützel <rethab@rethab.ch>
2014-04-12 18:53:36 +0200

    single main for tests

---
+++ test/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+#include "util.c"
+#include "stomp-test.c"
+#include "topic-test.c"
+
+int main(int argc, char **argv) {
+    install_segfault_handler();
+
+    assert(CUE_SUCCESS == CU_initialize_registry());
+
+    add_stomp_parse_suite();
+    add_stomp_create_suite();
+    topic_add_topic_suite();
+    topic_add_list_suite();
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return 0;
+}
