ee8d7ed165e9c800c722a096843ec5a9ddfe4aa9
Reto Hablützel <rethab@rethab.ch>
2014-04-16 19:26:35 +0200

    use worker in broker

--- src/broker.c
+++ src/broker.c
@@ -9,9 +9,12 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <assert.h>
+#include <pthread.h>
 
 #include "stomp.h"
 #include "topic.h"
+#include "worker.h"
 
 #define MAXPENDING 10
 #define BUFSIZE    1024
@@ -19,277 +22,9 @@
 #define EXIT    1
 #define NO_EXIT 0
 
-// TODO possibly move to worker.c
-struct worker_params {
-    /* the client to be handled */
-    int sockfd;
-
-    /* global list of topics */
-    struct list *topics;
-
-    /* global list of messages */
-    struct list *messages;
-};
-
 void show_error(int mode) {
     fprintf(stderr, "Error: %s\n", strerror(errno));
     if (mode == EXIT) exit(EXIT_FAILURE);
-}
-
-int read_command(int sock, struct stomp_command *cmd) {
-    printf("ENTERING: read_command\n");
-
-    int ret;
-
-    int pos;
-    char buf[BUFSIZE];
-    char c;
-
-    pos = 0;
-    while (pos < BUFSIZE) {
-        if (read(sock, &c, 1) != 1) {
-            show_error(NO_EXIT);
-            break;
-        }
-
-        buf[pos++] = c;
-
-        // end of command
-        if (c == '\0') {
-            break;
-        }
-    }
-
-    if (pos == BUFSIZE && buf[pos-1] != '\0') {
-        fprintf(stderr, "too much input\n");
-        return -1;
-    } else {
-        ret = parse_command(buf, cmd);
-
-        if (ret != 0)
-            fprintf(stderr, "Failed to parse: '%s': %d\n", buf, ret);
-
-        printf("EXITING: read_command\n");
-        return ret;
-    }
-}
-
-int send_command(int sock, struct stomp_command cmd) {
-    printf("ENTERING: send_command\n");
-
-    int ret, resplen;
-    char *resp;
-
-    ret = create_command(cmd, &resp);
-    if (ret != 0) {
-        fprintf(stderr, "Internal Error: %d\n", ret);
-        return -1;
-    }
-
-    resplen = strlen(resp);
-    ret = write(sock, resp, resplen);
-    free(resp);
-    if (ret == -1) {
-        show_error(NO_EXIT);
-        return -1;
-    }
-
-    printf("EXITING: send_command\n");
-
-    return 0;
-}
-
-int send_error(int sock, char *reason) {
-    printf("ENTERING: send_error\n");
-
-    int ret;
-    struct stomp_command respc;
-    struct stomp_header header;
-
-    respc.name = "ERROR";
-    header.key = "message";
-    header.val = reason;
-    respc.headers = &header;
-    respc.nheaders = 1;
-    respc.content = NULL;
-
-    ret = send_command(sock, respc);
-
-    printf("EXITING: send_error\n");
-
-    return ret;
-}
-
-int send_receipt(int sock) {
-    printf("ENTERING: send_receipt\n");
-
-    int ret;
-    struct stomp_command respc;
-
-    respc.name = "RECEIPT";
-    respc.headers = NULL;
-    respc.nheaders = 0;
-    respc.content = NULL;
-
-    ret = send_command(sock, respc);
-
-    printf("EXITING: send_receipt\n");
-
-    return ret;
-}
-
-int send_connected(int sock) {
-    printf("ENTERING: send_connected\n");
-
-    int ret; 
-    struct stomp_command respc;
-
-    respc.name = "CONNECTED";
-    respc.headers = NULL;
-    respc.nheaders = 0;
-    respc.content = NULL;
-
-    ret = send_command(sock, respc);
-
-    printf("EXITING: send_connected\n");
-    return ret;
-}
-
-int process_send(struct stomp_command cmd, struct worker_params params) {
-    printf("ENTERING: process_send\n");
-    int ret;
-
-    struct list *topics = params.topics;
-    struct list *messages = params.messages;
-    char *topic = cmd.headers[0].val; // has only one header
-    char *content = cmd.content;
-
-    ret = topic_add_message(topics, messages, topic, content);
-    if (ret != 0) {
-        fprintf(stderr, "Error from topic_add_message: %d\n", ret);
-        ret = send_error(params.sockfd, "Failed to add message");
-
-        if (ret != 0) fprintf(stderr, "Failed to send error\n");
-
-        return -1;
-    } else {
-        printf("Added message '%s' to topic '%s'\n", content, topic);
-        return 0;
-    }
-    printf("EXITING: process_send\n");
-}
-
-int process_subscribe(struct stomp_command cmd,
-        struct worker_params params, struct subscriber *sub) {
-    printf("ENTERING: process_subscribe\n");
-    int ret;
-
-    struct list *topics = params.topics;
-    char *topic = cmd.headers[0].val; // has only one header
-
-    ret = topic_add_subscriber(topics, topic, sub);
-    if (ret != 0) {
-        fprintf(stderr, "Error from topic_add_subscriber: %d\n", ret);
-        ret = send_error(params.sockfd, "Failed to add subscriber");
-
-        if (ret != 0) fprintf(stderr, "Failed to send error\n");
-
-        return -1;
-    } else {
-        printf("Added subscriber '%s' to topic '%s'\n", sub->name, topic);
-        return 0;
-    }
-    printf("EXITING: process_subscribe\n");
-}
-
-int process_disconnect(struct worker_params params,
-        struct subscriber *sub) {
-    printf("ENTERING: process_disconnect\n");
-    int ret;
-
-    struct list *topics = params.topics;
-    struct list *messages = params.messages;
-
-    // remove from topic
-    ret = topic_remove_subscriber(topics, sub);
-    if (ret != 0) {
-        fprintf(stderr, "Error from topic_remove_subscriber: %d\n", ret);
-        ret = send_error(params.sockfd,
-            "Failed to remove subscriber from topic");
-
-        if (ret != 0) fprintf(stderr, "Failed to send error\n");
-        return ret;
-
-    } else {
-        // remove from message
-        ret = message_remove_subscriber(messages, sub);
-        if (ret != 0) {
-            fprintf(stderr, "Error from message_remove_subscriber: %d\n", ret);
-            ret = send_error(params.sockfd, "Failed to add subscriber");
-
-            if (ret != 0) fprintf(stderr, "Failed to send error\n");
-
-            return -1;
-        } else {
-            printf("Removed subscriber '%s' from message\n", sub->name);
-
-            ret = send_receipt(params.sockfd);
-            if (ret != 0) {
-                ret = send_error(params.sockfd, "Failed to send receipt");
-
-                if (ret != 0) fprintf(stderr, "Failed to send error\n");
-                
-            }
-            return 0;
-        }
-    }
-
-
-    printf("EXITING: process_disconnect\n");
-}
-
-void handle_client(struct worker_params params) {
-    int ret;
-    int connected;
-    struct stomp_command cmd;
-    struct subscriber sub;
-
-    connected = 0;
-    while (1) {
-        ret = read_command(params.sockfd, &cmd);
-        if (ret != 0) {
-            ret = send_error(params.sockfd, "Failed to parse");
-            continue;
-        }
-
-        if (!connected) {
-            if (strcmp("CONNECT", cmd.name) != 0) {
-                ret = send_error(params.sockfd, "Expected CONNECT");
-                continue;
-            } else {
-                connected = 1;
-                sub.sockfd = params.sockfd;
-                sub.name = cmd.headers[0].val; // has only one header
-                ret = send_connected(params.sockfd);
-                continue;
-            }
-        } else {
-            if (strcmp("SEND", cmd.name) == 0) {
-                ret = process_send(cmd, params);
-            } else if (strcmp("SUBSCRIBE", cmd.name) == 0) {
-                ret = process_subscribe(cmd, params, &sub);
-            } else if (strcmp("DISCONNECT", cmd.name) == 0) {
-                ret = process_disconnect(params, &sub);
-            } else {
-                ret = send_error(params.sockfd, "Unexpected command");
-                fprintf(stderr, "Unexpected Command %s\n", cmd.name);
-            }
-                
-        }
-
-    }
-
-
 }
 
 int main(int argc, char** argv) {
@@ -329,7 +64,13 @@ int main(int argc, char** argv) {
             continue;
         }
 
-        struct worker_params params = {cli, &topics, &messages };
+        pthread_mutex_t mutex_w = PTHREAD_MUTEX_INITIALIZER;
+        pthread_mutex_t mutex_r = PTHREAD_MUTEX_INITIALIZER;
+        struct client client;
+        client.mutex_w = &mutex_w;
+        client.mutex_r = &mutex_r;
+        client.sockfd = cli;
+        struct worker_params params = {&client, &topics, &messages };
         handle_client(params);
     }
 
