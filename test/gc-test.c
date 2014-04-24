3ecc934b6a31bbc4f2c749638cbc4db114eff51f
Reto Hablützel <rethab@rethab.ch>
2014-04-24 06:37:08 +0200

    test for gc'ing twice on same list

--- test/gc-test.c
+++ test/gc-test.c
@@ -225,10 +225,10 @@ void test_gc_remove_eligible_msgs() {
 
     list_add(&eligible, &msg1);
     list_add(&eligible, &msg3);
-    list_add(&eligible, &msg6);
+    list_add(&eligible, &msg6); // not in messages
 
     ret = gc_remove_eligible_msgs(&messages, &eligible);
-    CU_ASSERT_EQUAL_FATAL(0, ret);
+    CU_ASSERT_EQUAL_FATAL(2, ret);
 
     CU_ASSERT_PTR_EQUAL_FATAL(messages.root->entry, &msg2);
     CU_ASSERT_PTR_EQUAL_FATAL(messages.root->next->entry, &msg4);
@@ -242,6 +242,91 @@ void test_gc_remove_eligible_msgs() {
     CU_ASSERT_PTR_NULL_FATAL(msg3.content);
     CU_ASSERT_PTR_NULL_FATAL(msg3.topicname);
     CU_ASSERT_PTR_NULL_FATAL(msg3.stats);
+}
+
+void test_gc_remove_eligible_msgs_twice() {
+    int ret;
+    struct list messages;
+    struct list eligible;
+
+    struct message msg1;
+    struct message msg2;
+    struct message msg3;
+    struct message msg4;
+    struct message msg5;
+    struct message msg6;
+
+    struct list *stats1 = malloc(sizeof(struct list));
+    struct list *stats2 = malloc(sizeof(struct list));
+    struct list *stats3 = malloc(sizeof(struct list));
+    struct list *stats4 = malloc(sizeof(struct list));
+    struct list *stats5 = malloc(sizeof(struct list));
+    struct list *stats6 = malloc(sizeof(struct list));
+    
+    list_init(&messages);   
+    list_init(&eligible);
+    message_init(&msg1);
+    message_init(&msg2);
+    message_init(&msg3);
+    message_init(&msg4);
+    message_init(&msg5);
+    message_init(&msg6);
+    list_add(&messages, &msg1);
+    list_add(&messages, &msg2);
+    list_add(&messages, &msg3);
+    list_add(&messages, &msg4);
+    list_add(&messages, &msg5);
+    list_add(&messages, &msg6);
+    list_init(stats1);
+    list_init(stats2);
+    list_init(stats3);
+    list_init(stats4);
+    list_init(stats5);
+    list_init(stats6);
+    msg1.stats = stats1;
+    msg1.content = NULL;
+    msg1.topicname = NULL;
+    msg2.stats = stats2;
+    msg2.content = NULL;
+    msg2.topicname = NULL;
+    msg3.stats = stats3;
+    msg3.content = NULL;
+    msg3.topicname = NULL;
+    msg4.stats = stats4;
+    msg4.content = NULL;
+    msg4.topicname = NULL;
+    msg5.stats = stats5;
+    msg5.content = NULL;
+    msg5.topicname = NULL;
+
+
+    // remove some
+    list_add(&eligible, &msg1);
+    list_add(&eligible, &msg3);
+    ret = gc_remove_eligible_msgs(&messages, &eligible);
+    CU_ASSERT_EQUAL_FATAL(2, ret);
+
+    // clean list
+    list_clean(&eligible);
+    list_init(&eligible);
+
+    // remove more
+    list_add(&eligible, &msg6);
+    list_add(&eligible, &msg2);
+    ret = gc_remove_eligible_msgs(&messages, &eligible);
+    CU_ASSERT_EQUAL_FATAL(2, ret);
+
+    // clean list
+    list_clean(&eligible);
+    list_init(&eligible);
+
+    // remove more
+    list_add(&eligible, &msg4);
+    list_add(&eligible, &msg5);
+    ret = gc_remove_eligible_msgs(&messages, &eligible);
+    CU_ASSERT_EQUAL_FATAL(2, ret);
+
+    CU_ASSERT_EQUAL_FATAL(0, list_len(&messages));
 }
 
 void test_gc_remove_eligible_stats() {
@@ -284,7 +369,7 @@ void test_gc_remove_eligible_stats() {
     list_add(&eligible, &stat22);
 
     ret = gc_remove_eligible_stats(&messages, &eligible);
-    CU_ASSERT_EQUAL_FATAL(0, ret);
+    CU_ASSERT_EQUAL_FATAL(3, ret);
 
     CU_ASSERT_PTR_EQUAL_FATAL(&stat13, msg1.stats->root->entry);
     CU_ASSERT_PTR_NULL_FATAL(msg1.stats->root->next);
@@ -296,6 +381,59 @@ void test_gc_remove_eligible_stats() {
     CU_ASSERT_PTR_NULL_FATAL(stat12.statrwlock);
     CU_ASSERT_PTR_NULL_FATAL(stat22.statrwlock);
 
+}
+
+void test_gc_remove_eligible_stats_twice() {
+    int ret;
+    struct list messages;
+    struct list eligible;
+
+    struct message msg1;
+    struct list stats1;
+    struct msg_statistics stat11;
+    struct msg_statistics stat12;
+    struct msg_statistics stat13;
+    struct msg_statistics stat21;
+    struct msg_statistics stat22;
+
+    struct message msg2;
+    struct list stats2;
+    
+    list_init(&messages);   
+    list_init(&stats1);
+    list_init(&stats2);
+    list_init(&eligible);
+    list_add(&messages, &msg1);
+    list_add(&messages, &msg2);
+    list_add(&stats1, &stat11);
+    list_add(&stats1, &stat12);
+    list_add(&stats1, &stat13);
+    list_add(&stats2, &stat21);
+    list_add(&stats2, &stat22);
+    msg_statistics_init(&stat11);
+    msg_statistics_init(&stat12);
+    msg_statistics_init(&stat13);
+    msg_statistics_init(&stat21);
+    msg_statistics_init(&stat22);
+    msg1.stats = &stats1;
+    msg2.stats = &stats2;
+
+    // remove some
+    list_add(&eligible, &stat11);
+    list_add(&eligible, &stat12);
+    list_add(&eligible, &stat22);
+    ret = gc_remove_eligible_stats(&messages, &eligible);
+    CU_ASSERT_EQUAL_FATAL(3, ret);
+
+    // clean list
+    list_clean(&eligible);
+    list_init(&eligible);
+
+    //remove more
+    list_add(&eligible, &stat13);
+    list_add(&eligible, &stat21);
+    ret = gc_remove_eligible_stats(&messages, &eligible);
+    CU_ASSERT_EQUAL_FATAL(2, ret);
 }
 
 void test_gc_run_gc() {
@@ -363,8 +501,12 @@ void gc_test_suite() {
         test_gc_collect_eligible_msgs); 
     CU_add_test(gcSuite, "test_gc_remove_eligible_msgs",
         test_gc_remove_eligible_msgs); 
+    CU_add_test(gcSuite, "test_gc_remove_eligible_msgs_twice",
+        test_gc_remove_eligible_msgs_twice); 
     CU_add_test(gcSuite, "test_gc_remove_eligible_stats",
         test_gc_remove_eligible_stats); 
+    CU_add_test(gcSuite, "test_gc_remove_eligible_stats_twice",
+        test_gc_remove_eligible_stats_twice); 
     CU_add_test(gcSuite, "test_gc_run_gc",
         test_gc_run_gc); 
 }
